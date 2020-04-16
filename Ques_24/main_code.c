a028f7f3991b2fcb7152f120a24cb82bb86aba4d
pranavkutty <pranavkutty13@gmail.com>
2020-04-16 11:41:13 +0530

    demand page update

--- Ques_24/main_code.c
+++ Ques_24/main_code.c
@@ -32,3 +32,46 @@ void main(){
 
 }while(swtch<3);
 }
+void userInput(){
+	
+	
+	printf("\nEnter service Page Fault [Empty|Page is not Modified][in milliseconds]");
+	scanf("%lf",&service_page_fault_empty);
+	printf("Enter Service Page Fault [Modified Page][in milliseconds]");
+	scanf("%lf",&service_page_fault_modified);
+	printf("Enter Memory Access Time[in nanoseconds]");
+	scanf("%lf",&mem_access_time);
+	printf("Enter Percentage of time the page to be replaced is modified[0-100]");
+	scanf("%lf",&times_page_modified);
+	printf("Enter Effective Access time[in nanoseconds]");
+	scanf("%lf",&effective_access_time);
+
+	service_page_fault_empty_ns = (service_page_fault_empty*1000000);
+	service_page_fault_modified_ns = (service_page_fault_modified*1000000);
+	times_page_modified_per = (times_page_modified/100);   
+	printf("\nPage Fault rate calculated For:\n");
+	printf("Service Page Fault[Empty|Page Not Modified]=%lf \n",service_page_fault_empty_ns);
+	printf("Service Page Fault [Modified Page][in nanoseconds] %lf \n",service_page_fault_modified_ns);
+	printf("Memory Access Time[in nanoseconds]%lf\n",mem_access_time);
+	printf("Effective Access Time %lf\n",effective_access_time);
+    pageFaultRate =  page_fault_rate(service_page_fault_empty_ns,service_page_fault_modified_ns,mem_access_time,times_page_modified_per,effective_access_time);
+	printf("\nMaximum Acceptable Page Fault rate = %.2e[exponential notation]",pageFaultRate);
+
+
+}
+
+double page_fault_rate(double servicePageFaultEmpty,double servicePageFaultMod,double memAccess,double timesPages,double effAccess){
+	double assume,serve;
+	double numErator,denOminator;
+	double pageFault;
+	 assume = (1- timesPages)*servicePageFaultEmpty;
+	 serve = timesPages*servicePageFaultMod;
+	 numErator = effAccess - memAccess;
+     denOminator = (assume+serve);
+
+	pageFault = numErator/denOminator;
+	return pageFault;
+
+
+}
+
